aa7c3fa3533987fdfa75269ba75a91bf04637227
Nitesh Yadav <ny09346@gmail.com>
2025-07-11 22:58:47 +0530

    commit for basics

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from streamlit-app!")
+import streamlit as st
 
+st.title("Hello, Streamlit!")
+st.subheader("This is a simple Streamlit app.")
+st.text("You can use this app to display text, images, and more.")
+st.write("Streamlit makes it easy to create web apps for data science and machine learning projects.")
 
-if __name__ == "__main__":
-    main()
+# selectbox for user input
+cars = st.selectbox(
+    'Choose an Car:',
+    ['select', 'BMW', 'Mercedes', 'Audi', 'Toyota', 'Honda']
+)
+st.write(f"You selected: {cars}")
+st.success("You have successfully selected a car!")
